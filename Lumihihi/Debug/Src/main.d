52fe58c691e8947deab134f1c410b0631ee31a63
LuongThuy05 <thuyluong051204@gmail.com>
2025-04-18 14:56:32 +0700

    Add files via upload

---
+++ Lumihihi/Debug/Src/main.d
@@ -0,0 +1,13 @@
+Src/main.o: ../Src/main.c \
+ D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h \
+ D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h \
+ D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h \
+ D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h
+
+D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos/timer.h:
+
+D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/Ucglib.h:
+
+D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib/ucg.h:
+
+D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
---
+++ Lumihihi/Debug/Startup/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Startup/startup_stm32f401retx.s 
+
+OBJS += \
+./Startup/startup_stm32f401retx.o 
+
+S_DEPS += \
+./Startup/startup_stm32f401retx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Startup/%.o: ../Startup/%.s Startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/button" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/buzzer" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/led" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/rtos" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/sensor" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/serial" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Middle/ucglib" -I"D:/TTCS/SDK_1.0.3_NUCLEO-F401RE/shared/Utilities" -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
